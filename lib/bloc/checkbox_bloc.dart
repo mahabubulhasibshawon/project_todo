b49d11153646342e49839d781726b45d7b1d6289
mahabubulhasibshawon <mahabubulhasibshawon@gmail.com>
2025-01-03 22:38:07 +0600

    checkbox

--- lib/bloc/checkbox_bloc.dart
+++ lib/bloc/checkbox_bloc.dart
@@ -5,6 +5,7 @@ import 'checkbox_state.dart';
 
 class CheckboxBloc extends Bloc<CheckboxEvent, CheckboxState> {
   CheckboxBloc() : super(CheckboxInitial()) {
+    // onclick checkbox reverse will be emitted
     on<CheckboxChanged>((event, emit) {
       emit(CheckboxChangedState(isChecked: !state.isChecked));
     });
